7f780cdb90be4835b4d8d59a7e287acb7a96e922
120140529 <63300440+120140529@users.noreply.github.com>
2022-03-27 16:43:18 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,58 +2,34 @@
 
 cat << EOF > /etc/v2ray/config.json
 {
-    "policy": {
-        "levels": {
-            "0": {
-                "handshake": 5,
-                "connIdle": 300,
-                "uplinkOnly": 2,
-                "downlinkOnly": 5,
-                "statsUserUplink": false,
-                "statsUserDownlink": false,
-                "bufferSize": 10240
-            }
-        },
-        "system": {
-            "statsInboundUplink": false,
-            "statsInboundDownlink": false,
-            "statsOutboundUplink": false,
-            "statsOutboundDownlink": false
-        }
-    },
-//    "inbounds": [
-//        {
-//           "port": $PORT,
-//            "protocol": "vless",
-//            "settings": {
-//                "clients": [
-//                    {
-//                        "id": "$UUID",
-//                        "level": 0
-//                    }
-//                ],
-//                 "decryption": "none"
-//            },
-//            "streamSettings": {
-//                "network": "ws",
-//                "security": "none"
-//            }
-//        }
-//    ],
-    
-    
-    	"inbounds": [
-		{
+	"policy": {
+		"levels": {
+			"0": {
+				"handshake": 5,
+				"connIdle": 300,
+				"uplinkOnly": 2,
+				"downlinkOnly": 5,
+				"statsUserUplink": false,
+				"statsUserDownlink": false,
+				"bufferSize": 10240
+			}
+		},
+		"system": {
+			"statsInboundUplink": false,
+			"statsInboundDownlink": false,
+			"statsOutboundUplink": false,
+			"statsOutboundDownlink": false
+		}
+	},
+	"inbounds": [{
 			"port": $PORT,
 			"protocol": "vmess",
 			"settings": {
-				"clients": [
-					{
-						"id": "$UUID",
-						"level": 1,
-						"alterId": 0
-					}
-				]
+				"clients": [{
+					"id": "$UUID",
+					"level": 1,
+					"alterId": 0
+				}]
 			},
 			"streamSettings": {
 				"network": "tcp",
@@ -99,17 +75,87 @@ cat << EOF > /etc/v2ray/config.json
 				]
 			}
 		}
-		//include_ss
-		//include_socks
-		//include_mtproto
-		//include_in_config
-		//
+
 	],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+	"outbounds": [{
+			"protocol": "freedom",
+			"settings": {
+				"domainStrategy": "UseIP"
+			},
+			"tag": "direct"
+		},
+		{
+			"protocol": "blackhole",
+			"settings": {},
+			"tag": "blocked"
+		},
+		{
+			"protocol": "mtproto",
+			"settings": {},
+			"tag": "tg-out"
+		}
+
+	],
+	"dns": {
+		"servers": [
+			"https+local://8.8.8.8/dns-query",
+			"8.8.8.8",
+			"1.1.1.1",
+			"localhost"
+		]
+	},
+	"routing": {
+		"domainStrategy": "IPOnDemand",
+		"rules": [{
+				"type": "field",
+				"ip": [
+					"0.0.0.0/8",
+					"10.0.0.0/8",
+					"100.64.0.0/10",
+					"127.0.0.0/8",
+					"169.254.0.0/16",
+					"172.16.0.0/12",
+					"192.0.0.0/24",
+					"192.0.2.0/24",
+					"192.168.0.0/16",
+					"198.18.0.0/15",
+					"198.51.100.0/24",
+					"203.0.113.0/24",
+					"::1/128",
+					"fc00::/7",
+					"fe80::/10"
+				],
+				"outboundTag": "blocked"
+			},
+			{
+				"type": "field",
+				"inboundTag": ["tg-in"],
+				"outboundTag": "tg-out"
+			},
+			{
+				"type": "field",
+				"domain": [
+					"domain:epochtimess.com",
+					"domain:shenyuns.com"
+				],
+				"outboundTag": "blocked"
+			},
+			{
+				"type": "field",
+				"protocol": [
+					"bittorrent"
+				],
+				"outboundTag": "blocked"
+			}
+		]
+	},
+	"transport": {
+		"kcpSettings": {
+			"uplinkCapacity": 100,
+			"downlinkCapacity": 100,
+			"congestion": true
+		}
+	}
 }
 EOF
 
